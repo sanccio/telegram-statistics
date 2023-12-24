5b1aa3cdc60f49c188196675dda56042e42c8615
sanchesko13 <sanchesko13@gmail.com>
2023-12-24 06:23:25 +0200

    remove console output for chat statistics (committed in March)

--- TelegramStatistics.CLI/Program.cs
+++ TelegramStatistics.CLI/Program.cs
@@ -10,7 +10,8 @@ string reportSavePath = Path.Combine(Environment.GetFolderPath(Environment.Speci
 Console.OutputEncoding = Encoding.UTF8;
 
 bool isInputPathValid = false;
-Console.WriteLine("Enter the file path. Example: C:\\Documents\\Telegram Desktop\\result.json\n");
+Console.WriteLine("To get statistics, export your Telegram chat history in .json format without specifying any parameters.\n");
+Console.WriteLine("Enter the file path. Example: C:\\Documents\\Telegram Desktop\\ChatExport_XXXX-XX-XX\\result.json\n");
 
 while(!isInputPathValid)
 {
@@ -19,16 +20,14 @@ while(!isInputPathValid)
     if (File.Exists(telegramChatFilePath) && Path.GetExtension(telegramChatFilePath).Equals(".json", StringComparison.OrdinalIgnoreCase))
     {
         isInputPathValid = true;
-        Console.Write("\nGreat! Your file report will be on the desktop. Press any key to continue...");
-        Console.ReadKey();
+        Console.Write("\nFile found!\n");
+        //Console.ReadKey();
     }
     else
     {
         Console.WriteLine("\nOops! The file path you entered either does not exist or does not point to a valid JSON file. Please check your input and try again.\n");
     }
 }
-
-Console.Clear();
 
 
 IDeserializer deserializer = new JsonDeserializer();
@@ -38,23 +37,28 @@ ITextAnalyzer textAnalyzer = new TextAnalyzer();
 IChatStatistics chatStatistics = new ChatStatistics(chatService, textAnalyzer);
 
 
-ConsoleOutput.PrintChatInfo(chat);
+Console.WriteLine("\nTo start analyzing the chat, press any key... This may take a moment.");
+Console.ReadKey();
+
+
+//ConsoleOutput.PrintChatInfo(chat);
 
 
 Dictionary<string, int> userMessageCounts = chatStatistics.GetMessageCountOfEverySender(chat);
-ConsoleOutput.PrintUserMessagesNumber(userMessageCounts);
+//ConsoleOutput.PrintUserMessagesNumber(userMessageCounts);
 
 
 const int minimumWordFrequency = 1;
 IEnumerable<WordCount> wordCounts = chatStatistics.GetWordsUsage(chat.Messages!, minimumWordFrequency);
-ConsoleOutput.PrintTable(wordCounts/*.Take(10).ToList()*/); // Remove .Take()
+//ConsoleOutput.PrintTable(wordCounts);
 
 
 IEnumerable<UserWordCount> userWordCounts = chatStatistics.GetWordsUsagePerUser(chat, minimumWordFrequency);
-ConsoleOutput.PrintTable(userWordCounts);
+//ConsoleOutput.PrintTable(userWordCounts);
 
 
 TXTFileWriter fileWriter = new();
 fileWriter.WriteFile(chat, reportSavePath, wordCounts, userWordCounts, userMessageCounts);
 
+Console.WriteLine("\nDone! Your file report should now appear on the desktop. You can close the app.");
 Console.Read();
